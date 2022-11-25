8c01c40dde85dffaf4759cb1cc2dc1b769c98e39
MichaPehlivan <michapehlivan2004@gmail.com>
2022-11-25 15:23:54 +0100

    sending process logs to channels

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::{env, process::Stdio};
 
-use serenity::{prelude::*, async_trait, model::prelude::Ready};
-use tokio::{process::Command, io::{BufReader, AsyncBufReadExt}};
+use serenity::{prelude::*, async_trait, model::prelude::{Ready, ChannelId, Message}};
+use tokio::{process::Command, io::{BufReader, AsyncBufReadExt}, time::{sleep, Duration}};
 
 struct ProgramArgs;
 
 impl TypeMapKey for ProgramArgs {
     type Value = Vec<String>;
+}
+
+struct ChannelList;
+
+impl TypeMapKey for ChannelList {
+    type Value = Vec<ChannelId>;
 }
 
 struct Handler;
@@ -15,6 +21,11 @@ struct Handler;
 impl EventHandler for Handler {
     async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
+
+        while ctx.data.read().await.get::<ChannelList>().unwrap().is_empty() {
+            sleep(Duration::from_micros(1)).await;
+        }
+
         let mut cmd = Command::new("bash")
                                 .args(ctx.data.read().await.get::<ProgramArgs>().unwrap())
                                 .stdout(Stdio::piped())
@@ -24,15 +35,39 @@ impl EventHandler for Handler {
         let stdout = cmd.stdout.take().expect("command did not have handle to stdout");
         let mut reader = BufReader::new(stdout).lines();
 
+        let ctx_clone = ctx.clone();
         tokio::spawn(async move {
             let status = cmd.wait().await
                 .expect("process encountered an error");
 
             println!("process exit status was: {}", status);
+            
+            for channel in ctx.data.read().await.get::<ChannelList>().unwrap().iter() {
+
+                if let Err(why) = channel.say(&ctx.http, format!("process exited with status code {}", status)).await {
+                    println!("Error sending message: {:?}", why);
+                }
+            }
         });
 
         while let Some(line) = reader.next_line().await.unwrap() {
-            println!("{}", line);
+            for channel in ctx_clone.data.read().await.get::<ChannelList>().unwrap().iter() {
+
+                if let Err(why) = channel.say(&ctx_clone.http, &line).await {
+                    println!("Error sending message: {:?}", why);
+                }
+            }
+        }
+    }
+
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!log" {
+            let mut data = ctx.data.write().await;
+            data.get_mut::<ChannelList>().unwrap().push(msg.channel_id);
+            
+            if let Err(why) = msg.channel_id.say(&ctx.http, "now posting logs in this channel").await {
+                println!("Error sending message: {:?}", why);
+            }
         }
     }
 }
@@ -52,6 +87,7 @@ async fn main() {
     {
         let mut data = client.data.write().await;
         data.insert::<ProgramArgs>(args);
+        data.insert::<ChannelList>(Vec::new());
     }
 
     if let Err(why) = client.start().await {
